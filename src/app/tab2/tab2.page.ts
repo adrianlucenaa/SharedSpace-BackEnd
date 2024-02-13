33d5d0714cf0aacd16df8f946cbc9a789998c9ed
Adrian Lucena <adrianlucenavaro@gmail.com>
2024-02-13 09:22:50 +0100

    Component Apartment creado y boton funcionando

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem, IonList, IonCard } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { ApartmentComponent } from '../apartment/apartment.component';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonCard, IonList, IonItem, IonButton, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent,ApartmentComponent]
 })
 export class Tab2Page {
+
+  apartmentVisible: boolean = false;
+
+  navigateToApartment() {
+    this.apartmentVisible = !this.apartmentVisible;
+  }
 
   constructor() {}
 
