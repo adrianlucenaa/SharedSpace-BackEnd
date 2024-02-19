7f8c9af12a9e910353c488b1970b80fab2c0e422
Sergio Cubero <scubcar521@gmail.es>
2024-02-19 14:51:34 +0100

    Front-end user 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { Tab1Page } from './tab1/tab1.page';
 import { Tab2Page } from './tab2/tab2.page';
 import { Tab3Page } from './tab3/tab3.page';
 import { EdituserComponent } from './edituser/edituser.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 
 export const routes: Routes = [
@@ -14,6 +15,7 @@ export const routes: Routes = [
   { path: 'login', component: LoginPage },
   { path: 'register', component: RegisterComponent },
   {path: 'edituser', component: EdituserComponent},
+  {path: 'tasks', component: TasksComponent},
   {
     path: 'tabs',
     loadChildren: () => import('./tabs/tabs.routes').then(m => m.routes)
---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,19 @@
+import { Component} from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.scss'],
+  standalone: true,
+  imports:[IonicModule],
+})
+export class TasksComponent{
+  
+
+}
+  
+
+
+
+
