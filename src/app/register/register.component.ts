a2ed8c9ef0476faf17b8a8a990f7a0c654337189
Sergio Cubero <scubcar521@gmail.es>
2024-03-04 11:28:40 +0100

    vista tareas y mas actualizaciones

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,22 +1,39 @@
 import { CommonModule } from '@angular/common';
-import { Component} from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
   standalone: true,
-  imports: [CommonModule, IonicModule ]
+  imports: [CommonModule, IonicModule],
+  providers: [UserService]
 })
 export class RegisterComponent {
+
+  UserService = inject(UserService);
+  userVisible : boolean = false;
+  user:any;
 
 
   constructor(private router: Router) { }
  
   redirectToLogin() {
     this.router.navigate(['login']); // Redirige a la página de inicio de sesión
+  }
+  saveChanges() {
+    this.UserService.createUser(this.user).subscribe((createduser) => {
+     console.log('Nuevo usuario creado:', createduser);
+     this.user = createduser; 
+    });
+    /**this.UserService.updateUser(this.user).subscribe((updateduser) => {
+      console.log('Usuario actualizado:', updateduser);
+      this.user = updateduser;
+    });**/
+    
   }
   
 
---
+++ src/app/user-info/user-info.component.ts
@@ -0,0 +1,19 @@
+/**import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-info',
+  templateUrl: './user-info.component.html',
+  styleUrls: ['./user-info.component.scss'],
+})
+export class UserInfoComponent  {
+
+  constructor(public taskS: TaskService,
+    public modalController: ModalController,
+    private alertController: AlertController) {
+      console.log("CONS")
+     }
+
+ 
+
+}
+**/
