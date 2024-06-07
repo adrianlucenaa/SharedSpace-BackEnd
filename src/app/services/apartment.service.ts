fb19ee512ecb0d6ec4622b91746d645e8ed82f13
SeRGiMeLLaMo <sergiocube2003@gmail.com>
2024-06-07 18:35:37 +0200

    commit (07/06/24  18:35)

--- src/app/services/apartment.service.ts
+++ src/app/services/apartment.service.ts
@@ -2,18 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Apartment } from '../model/apartment.model';
-import { environment } from 'src/environments/environment';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApartmentService {
+  private apiUrl = 'http://localhost:8081/apartments';
 
-    private apiUrl = 'http://localhost:8081/apartments';
-
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getApartments(): Observable<Apartment[]> {
     return this.http.get<Apartment[]>(this.apiUrl);
@@ -21,16 +17,6 @@ export class ApartmentService {
 
   getApartmentById(id: number): Observable<Apartment> {
     return this.http.get<Apartment>(`${this.apiUrl}/${id}`);
-  }
-
-  /*
-  assignApartmentToUser(apartmentId: number, userId: number): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}/${apartmentId}/assign`, { userId });
-  }
-  */
-
-  getApartmentByUser(userId: any): Observable<Apartment> {
-    return this.http.get<Apartment>(`${this.apiUrl}/${userId}`);
   }
 
   createApartment(apartment: Apartment): Observable<Apartment> {
@@ -45,12 +31,10 @@ export class ApartmentService {
     return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 
-  //Metodo para buscar un apartmento por nombre de apartment
   getApartmentByName(name: string): Observable<Apartment> {
     return this.http.get<Apartment>(`${this.apiUrl}/name/${name}`);
   }
 
-  //Metodo para actulizar un usuario y añadirle un apartmento
   assignApartmentToUser(apartmentId: number, userId: number): Observable<any> {
     return this.http.put<any>(`${this.apiUrl}/${apartmentId}/assign`, { userId });
   }
