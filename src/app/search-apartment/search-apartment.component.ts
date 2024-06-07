276c5b5521fb0fbdaba53c541a4c5b182da94690
SeRGiMeLLaMo <sergiocube2003@gmail.com>
2024-06-07 20:32:35 +0200

    commit(07/06/24   20:32)

--- src/app/search-apartment/search-apartment.component.ts
+++ src/app/search-apartment/search-apartment.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { ApartmentService } from '../services/apartment.service';
 import { HttpClient } from '@angular/common/http';
-import {IonicModule} from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms'; // Asegúrate de importar FormsModule
 import { Apartment } from '../model/apartment.model';
 
 @Component({
@@ -11,29 +12,53 @@ import { Apartment } from '../model/apartment.model';
   templateUrl: './search-apartment.component.html',
   styleUrls: ['./search-apartment.component.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule],
+  imports: [IonicModule, CommonModule, FormsModule], // Añadir FormsModule aquí
 })
 export class SearchApartmentComponent {
-
+  apartmentName: string = '';
   apartment: Apartment | null = null;
 
   constructor(
     private router: Router,
-    private apartmentService: ApartmentService,
-    private http: HttpClient
+    private apartmentService: ApartmentService
   ) {}
-    
-  
 
-/*
-  enterToApartment() {
-    this.apartmentService.getApartmentByName(this.apartment).subscribe((getApartment) => {
-      console.log('Apartamento encontrado:', getApartment);
-      this.apartment = getApartment;
-      
-      this.updateLocalStorage();
-    });
+  searchApartment() {
+    this.apartmentService.getApartmentByName(this.apartmentName).subscribe(
+      (apartment) => {
+        if (apartment) {
+          console.log('Apartamento encontrado:', apartment);
+          this.apartment = apartment;
+          if (this.apartment.id !== undefined) {
+            this.assignApartmentToUser(this.apartment.id);
+          } else {
+            console.error('El ID del apartamento es undefined.');
+          }
+        } else {
+          console.log('Apartamento no encontrado.');
+        }
+      },
+      (error) => {
+        console.error('Error al buscar apartamento:', error);
+        // Aquí puedes mostrar un mensaje de error al usuario si lo deseas
+      }
+    );
   }
-  */
-  
+
+  assignApartmentToUser(apartmentId: number) {
+    const userId = parseInt(localStorage.getItem('id')!, 10);
+    this.apartmentService.assignApartmentToUser(apartmentId, userId).subscribe(
+      (response) => {
+        console.log('Apartamento asignado exitosamente:', response);
+        // Actualizar localStorage con el nuevo apartmentId
+        localStorage.setItem('apartmentId', apartmentId.toString());
+        // Redirigir a otra página si es necesario
+        this.router.navigate(['tabs/tab2']);
+      },
+      (error) => {
+        console.error('Error al asignar apartamento:', error);
+      }
+    );
+  }
 }
+
